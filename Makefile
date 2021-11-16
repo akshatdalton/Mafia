7427b26e9484175413f2cf9ab7b7d8c6c24397bd
ashirwad-maker <ashirwadsinha.com>
2021-11-16 15:34:18 +0530

    Added code for web_client

--- Makefile
+++ Makefile
@@ -2,12 +2,18 @@ CC = gcc
 
 CFLAGS = -g -Wall
 
-EXECUTABLES = web_server
+
+
+EXECUTABLES = web_server web_client
 
 all: ${EXECUTABLES}
 
 web_server: web_server.o connection.o
 	${CC} ${CFLAGS} -o web_server web_server.o connection.o
+
+web_client: web_client.o connection.o
+	${CC} ${CFLAGS} -o web_client -pthread web_client.o connection.o
+
 
 clean:
 	rm -rf *.o ${EXECUTABLES}
--- web_server.c
+++ web_server.c
@@ -1,14 +1,17 @@
 #include "connection.h"
 #include <strings.h>
 
-int main(int argc, char *argv[]) {
+int main(int argc, char *argv[])
+{
     int listen_fd, conn_fd;
 
-    if ((listen_fd = open_server_connection()) < 0) {
+    if ((listen_fd = open_server_connection()) < 0)
+    {
         err_n_die("open_server_connection error.");
     }
 
-    while (1) {
+    while (1)
+    {
         struct sockaddr_in client_addr;
         socklen_t addr_len = sizeof(client_addr);
 
@@ -16,7 +19,8 @@ int main(int argc, char *argv[]) {
         // it returns a "file descriptor" to the connection.
         printf("waiting for a connection on port %d\n", SERVER_PORT);
         fflush(stdout);
-        if ((conn_fd = accept(listen_fd, (SA *) &client_addr, (SA *) &addr_len)) < 0) {
+        if ((conn_fd = accept(listen_fd, (SA *)&client_addr, (SA *)&addr_len)) < 0)
+        {
             err_n_die("accept error.");
         }
 
